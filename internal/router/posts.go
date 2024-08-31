514c102ed3aa80fd39b4b84e7242aa061d1b9fa9
zimmah <zimmah@gmail.com>
2024-08-31 15:21:50 -0500

    add limit query param

--- internal/router/posts.go
+++ internal/router/posts.go
@@ -1,14 +1,25 @@
 package router
 
 import (
-	"github.com/zimmah/rss-aggregator/internal/database"
 	"net/http"
+	"strconv"
+
+	"github.com/zimmah/rss-aggregator/internal/database"
 )
 
 func (cfg *ApiConfig) handleGetPosts(w http.ResponseWriter, r *http.Request) {
+	queryParams := r.URL.Query()
+	limit, err := strconv.Atoi(queryParams.Get("limit"))
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error parsing query params")
+	}
+	if limit == 0 {
+		limit = 20
+	}
+
 	params := database.GetPostsByUserParams{
 		UserID: cfg.user.ID,
-		Limit:  20,
+		Limit:  int32(limit),
 	}
 	posts, err := cfg.DB.GetPostsByUser(r.Context(), params)
 	if err != nil {
